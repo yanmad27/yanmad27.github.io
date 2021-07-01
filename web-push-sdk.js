e714d9d6ebfdefe60d9e6e611fd3b511d7d2f68f
Doan Phan <doan.phan@primedata.ai>
2021-07-01 15:32:12 +0700

    demo worker

--- web-push-sdk.js
+++ web-push-sdk.js
@@ -176,15 +176,3 @@ wps.initContext = function (config) {
   }();
 
 }(pushOpts);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./js/worker-sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
